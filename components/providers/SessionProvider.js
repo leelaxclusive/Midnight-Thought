c57e240951f6969eeb5c7b6bd86c16ed50a0cfee
leelaxclusive <leelaxclusive@gmail.com>
2025-09-25 22:31:15 +0530

    Changes And support page added.

--- components/providers/SessionProvider.js
+++ components/providers/SessionProvider.js
@@ -3,7 +3,11 @@ import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react'
 
 export default function SessionProvider({ children, session }) {
   return (
-    <NextAuthSessionProvider session={session}>
+    <NextAuthSessionProvider
+      session={session}
+      refetchInterval={0}
+      refetchOnWindowFocus={false}
+    >
       {children}
     </NextAuthSessionProvider>
   )
